6513f75a47e25710e62ed62decb0287c7786c347
DomDuxbury <domduxbury@hotmail.com>
2017-02-09 21:32:58 +0000

    Initial commit

---
+++ postgres.py
@@ -0,0 +1,17 @@
+import psycopg2
+import json
+
+class Connection():
+
+    def __init__(self):
+
+        with open('./private/db_config.json') as config_file:    
+            db_config = json.load(config_file)
+
+        self.conn = psycopg2.connect(**db_config)
+
+    def query(self, sql):
+
+        curr = self.conn.cursor()
+        curr.execute(sql)
+        return curr.fetchall()
---
+++ schedule.py
@@ -0,0 +1,35 @@
+import postgres
+import datetime
+import trollius as asyncio
+import logging
+# from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+def main():
+    
+    logging.basicConfig()
+    db = postgres.Connection()
+    start_scheduler(db)
+        
+
+def start_scheduler(db):
+    
+    sched = AsyncIOScheduler()
+    sched.daemonic = False
+    sched.add_job(process_tweets, 'cron', second=0, args = [db])
+    sched.add_job(process_tweets, 'cron', second=0, args = [db])
+    sched.start()
+    
+    try:
+        asyncio.get_event_loop().run_forever()
+    except (KeyboardInterrupt, SystemExit):
+        pass
+
+def process_tweets(db): 
+    
+    result = db.query("SELECT COUNT(*) FROM tweets")
+    print result[0][0]
+
+
+if __name__ == "__main__":
+    main()
